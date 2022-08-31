ebdc3d0a1021b7546b368736fd4d4f05948a66be
yeigensatz <yannick_eigensatz@sluz.ch>
2022-08-31 19:17:22 +0200

    added daisyui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,47 @@
 import '../styles/globals.css'
+import { useEffect } from "react"
+import { themeChange } from "theme-change";
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const themeValues = [
+    "Dark",
+    "Cupcake",
+    "Bumblebee",
+    "Emerald",
+    "Corporate",
+    "Synthwave",
+    "Retro",
+    "Cyberpunk",
+    "Valentine",
+    "Halloween",
+    "Garden",
+    "Forest",
+    "Aqua",
+    "Lofi",
+    "Pastel",
+    "Fantasy",
+    "Wireframe",
+    "Black",
+    "Luxury",
+    "Dracula",
+    "Cmyk"
+  ]
+
+  useEffect(() => {
+    themeChange(false)
+  });
+  return (
+    <>
+      <select className="text-primary" data-choose-theme>
+        <option className="text-primary" option value="">Default Value</option>
+        {themeValues.map((value) => (
+          <option className="text-primary" key={value.toLowerCase()} value={value.toLowerCase()}>{value}</option>
+        ))}
+      </select>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
